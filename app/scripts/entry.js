51809ff38ee3ad9ebd350563e6eee4151a02f540
Caryn Ligon <carynligon@gmail.com>
2016-09-11 15:13:13 -0500

    working on svg based line graph, not working yet

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -1,20 +1,79 @@
-"use strict";
-var $ = require('jquery');
+// "use strict";
+// var $ = require('jquery');
+//
+//   d3.select('body').selectAll('div')
+//   .data([ 5, 10, 15, 20, 25])
+//   // whenever it finds a data element that doesn't have a ui element
+//   .enter().append('div')
+//   .attr('class', 'bar')
+//   .style('height', function(d) {
+//     return d * 5 + 'px';
+//   });
+//
+// $('.bar').hover((e) => {
+//   $('.showValue').remove();
+//   let value = (parseFloat(e.target.style.height) / 5);
+//   $(e.target).append("<div class=showValue>" + value + "</div>");
+// })
 
-  d3.select('body').selectAll('div')
-  .data([ 5, 10, 15, 20, 25])
-  // whenever it finds a data element that doesn't have a ui element
-  .enter().append('div')
-  .attr('class', 'bar')
-  .style('height', function(d) {
-    return d * 5 + 'px';
-  });
+// Set the dimensions of the canvas / graph
+var margin = {top: 30, right: 20, bottom: 30, left: 50},
+    width = 600 - margin.left - margin.right,
+    height = 270 - margin.top - margin.bottom;
 
-$('.bar').hover((e) => {
-  $('.showValue').remove();
-  let value = (parseFloat(e.target.style.height) / 5);
-  $(e.target).append("<div class=showValue>" + value + "</div>");
-})
+// Set the ranges
+var x = d3.time.scale().range([0, width]);
+var y = d3.scale.linear().range([height, 0]);
+
+// Define the axes
+var xAxis = d3.svg.axis().scale(x)
+    .orient("bottom").ticks(5);
+
+var yAxis = d3.svg.axis().scale(y)
+    .orient("left").ticks(5);
+
+// Define the line
+var valueline = d3.svg.line()
+    .x(function(d) { return x(d.date); })
+    .y(function(d) { return y(d.close); });
+
+// Adds the svg canvas
+var svg = d3.select("body")
+    .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+// Get the data
+d3.csv("https://s3.amazonaws.com/ed-college-choice-public/Most+Recent+Cohorts+(Scorecard+Elements).csv", function(error, data) {
+    data.forEach(function(d) {
+        d.name = d.INSTNM;
+        d.SATAVG = +d.SAT_AVG;
+    });
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function(d) { return d.name; }));
+    y.domain([0, d3.max(data, function(d) { return d.SATAVG; })]);
+
+    // Add the valueline path.
+    svg.append("path")
+        .attr("class", "line")
+        .attr("d", valueline(data));
+
+    // Add the X Axis
+    svg.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + height + ")")
+        .call(xAxis);
+
+    // Add the Y Axis
+    svg.append("g")
+        .attr("class", "y axis")
+        .call(yAxis);
+
+});
 
 
 // Using D3:
